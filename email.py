54c905cc6f36c83a8aaceacca1c3f26851f868f1
Stephanie Hosang <stephanie.hosang@scotiabank.com>
2018-10-12 11:27:32 -0400

    update

--- email.py
+++ email.py
@@ -2,21 +2,23 @@
 """
 Created on Fri Oct 12 11:10:49 2018
 
-@author: SH
+@author: s3856366
 """
 
 import win32com.client as win32
+import psutil
+import os
+import subprocess
 
-# Drafting and sending email notification to senders. You can add other senders’ email in the list
 def send_notification():
     outlook = win32.Dispatch('Outlook.Application')
     mail = outlook.CreateItem(0x0)
-    mail.To = 'sh@sh.com'
-    mail.Cc='sh@sh.com'
-    mail.Subject = 'Subject'
-    mail.body = "Body. \nBody. \n\nThank you."
+    mail.To = 'Stephanie.Hosang@scotiabank.com; Stephanie.Hosang@scotiabank.com'
+    mail.Cc='Stephanie.Hosang@scotiabank.com'
+    mail.Subject = 'Pending QC Report - Awaiting Case Transitions'
+    mail.body = "Please note that the Case Transitions file has not yet been updated. \nAutomated script run has been terminated. \n\nScript will need to be run manually once file is received."
     mail.send
-    print('Email sent!')
+    print('Notification email sent!')
     
 send_notification()
 
