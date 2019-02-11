a54ebbf293be6ec8a28c087d1fb368eb3cb261a2
Stephanie Hosang <stephgk27@hotmail.com>
2019-02-10 19:58:02 -0500

    Update email.py

--- email.py
+++ email.py
@@ -2,7 +2,7 @@
 """
 Created on Fri Oct 12 11:10:49 2018
 
-@author: s3856366
+@author: SH
 """
 
 import win32com.client as win32
@@ -13,10 +13,10 @@ import subprocess
 def send_notification():
     outlook = win32.Dispatch('Outlook.Application')
     mail = outlook.CreateItem(0x0)
-    mail.To = 'Stephanie.Hosang@scotiabank.com; Stephanie.Hosang@scotiabank.com'
-    mail.Cc='Stephanie.Hosang@scotiabank.com'
-    mail.Subject = 'Pending QC Report - Awaiting Case Transitions'
-    mail.body = "Please note that the Case Transitions file has not yet been updated. \nAutomated script run has been terminated. \n\nScript will need to be run manually once file is received."
+    mail.To = 'email@email.com; email@email.com'
+    mail.Cc='email@email.com'
+    mail.Subject = 'Subject'
+    mail.body = "Message Here. \nMessage Here."
     mail.send
     print('Notification email sent!')
     
